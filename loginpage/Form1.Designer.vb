2292d55c3e29d9c0a54b2e06f9b8e72bf50af371
saif-shayeb <sshayep@gmail.com>
2026-05-02 11:30:16 +0300

    first commit of login app

---
+++ loginpage/Form1.Designer.vb
@@ -0,0 +1,113 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(71, 168)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(172, 41)
+        Label1.TabIndex = 0
+        Label1.Text = "User Name:"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label2.Location = New Point(93, 235)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(150, 41)
+        Label2.TabIndex = 1
+        Label2.Text = "Password:"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Font = New Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        TextBox1.Location = New Point(249, 168)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(326, 47)
+        TextBox1.TabIndex = 2
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Font = New Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        TextBox2.Location = New Point(249, 232)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(326, 47)
+        TextBox2.TabIndex = 3
+        ' 
+        ' Button1
+        ' 
+        Button1.Font = New Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button1.Location = New Point(428, 523)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(147, 69)
+        Button1.TabIndex = 4
+        Button1.Text = "Login"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Font = New Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button2.Location = New Point(249, 523)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(147, 69)
+        Button2.TabIndex = 5
+        Button2.Text = "Register"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(614, 604)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+
+End Class
---
+++ loginpage/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Dim Username As String = "admin"
+    Dim Password As String = "password"
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If String.IsNullOrEmpty(TextBox1.Text.Trim) Or String.IsNullOrEmpty(TextBox2.Text.Trim) Then
+            MessageBox.Show("Please enter both username and password.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End If
+        If TextBox1.Text.Trim = Username And TextBox2.Text.Trim = Password Then
+            MessageBox.Show("welcome " + Username, "Success", MessageBoxButtons.OK)
+            Return
+        ElseIf Not TextBox1.Text.Trim = Username Or Not TextBox2.Text.Trim = Password Then
+            MessageBox.Show("Invalid User name or Password.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+
+    End Sub
+End Class
